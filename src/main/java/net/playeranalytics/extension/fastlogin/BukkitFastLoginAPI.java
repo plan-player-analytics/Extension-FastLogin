b712359a88f6fe81bc772eccb82bc761d71eef80
Aurora Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2024-03-10 10:25:31 +0200

    R1.3: Increase wait time

--- src/main/java/net/playeranalytics/extension/fastlogin/BukkitFastLoginAPI.java
+++ src/main/java/net/playeranalytics/extension/fastlogin/BukkitFastLoginAPI.java
@@ -2,6 +2,7 @@ package net.playeranalytics.extension.fastlogin;
 
 import com.djrapitops.plan.extension.NotReadyException;
 import com.github.games647.fastlogin.bukkit.FastLoginBukkit;
+import com.github.games647.fastlogin.core.PremiumStatus;
 import org.bukkit.Bukkit;
 
 import java.util.UUID;
@@ -16,13 +17,20 @@ public class BukkitFastLoginAPI {
     }
 
     public String getPremiumStatus(UUID playerUUID) {
-        // Need to wait for the status to become available since the API doesn't provide a Future.
-        // https://github.com/plan-player-analytics/Plan/issues/3485
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        PremiumStatus status;
+        int attempts = 0;
+        while (true) {
+            status = plugin.getStatus(playerUUID);
+            if (status != PremiumStatus.UNKNOWN || attempts > 9) break;
+            // Need to wait for the status to become available since the API doesn't provide a Future.
+            // https://github.com/plan-player-analytics/Plan/issues/3485
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            attempts++;
         }
-        return plugin.getStatus(playerUUID).getReadableName();
+        return status.getReadableName();
     }
 }
